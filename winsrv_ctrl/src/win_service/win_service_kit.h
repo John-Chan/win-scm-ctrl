ed7047d79e25a684f71e21b170f0999e5742b675
John-Chan <cpp.cheen@gmail.com>
2015-05-05 12:56:28 +0800

    disable warning c4800

--- winsrv_ctrl/src/win_service/win_service_kit.h
+++ winsrv_ctrl/src/win_service/win_service_kit.h
@@ -1,9 +1,14 @@
 #ifndef win_service_kit_h__
 #define win_service_kit_h__
 
+#pragma warning(push)
+#pragma warning(disable: 4244) // forcing value to bool 'true' or 'false' (performance warning)
+
 #include "sc_manager.h"
 #include "svc_infomation.h"
 #include "win_service.h"
 #include "winsrv_helper.hpp"
+
+#pragma warning(pop)
 
 #endif // win_service_kit_h__
--- winsrv_ctrl/src/win_service/winsrv_helper.hpp
+++ winsrv_ctrl/src/win_service/winsrv_helper.hpp
@@ -4,7 +4,24 @@
 #include <Windows.h>
 namespace WinService
 {
+	namespace detail{
+		class local_free_on_destruction
+		{
+		public:
+			explicit local_free_on_destruction(void* p)
+				: p_(p) {}
 
+			~local_free_on_destruction()
+			{
+				::LocalFree(p_);
+			}
+
+		private:
+			void* p_;
+			local_free_on_destruction(const local_free_on_destruction&);  // = deleted
+			local_free_on_destruction& operator=(const local_free_on_destruction&);  // = deleted
+		};
+	}//namespace detail
 
 template <class T>
 class buffer
@@ -49,19 +66,45 @@ static bool	invalid_svc_handle(SC_HANDLE handle)
 	return handle==NULL;
 }
 
+//////////////////////////////////////////////////////////////////////////
+
+class sys_error_category
+{
+public:
+	static std::string message( int ev )
+	{
+		LPVOID lpMsgBuf = 0;
+		DWORD retval = ::FormatMessageA( 
+			FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+			FORMAT_MESSAGE_FROM_SYSTEM | 
+			FORMAT_MESSAGE_IGNORE_INSERTS,
+			NULL,
+			ev,
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
+			(LPSTR) &lpMsgBuf,
+			0,
+			NULL 
+			);
+		detail::local_free_on_destruction lfod(lpMsgBuf);
+		if (retval == 0)
+			return std::string("Unknown error");
+
+		std::string str( static_cast<LPCSTR>(lpMsgBuf) );
+
+		while ( str.size()&& (str[str.size()-1] == '\n' || str[str.size()-1] == '\r') )
+		{	str.erase( str.size()-1 );}
+		if ( str.size() && str[str.size()-1] == '.' ) 
+		{ str.erase( str.size()-1 ); }
+		return str;
+	}
+	static std::string category(  ){
+		return "windows";
+	}
+};
+
 static std::string	get_sys_err_msg(unsigned int err_code)
 {
-	static const unsigned int char_count=512;
-	char msg_buff[char_count]={0};
-	::FormatMessageA(
-		FORMAT_MESSAGE_FROM_SYSTEM |
-		FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL,
-		err_code,
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		msg_buff,
-		(unsigned long)char_count, NULL );
-	return std::string(msg_buff);
+	return sys_error_category::message(err_code);
 }
 static int	close_svc_handle(SC_HANDLE handle,std::string& what_wrong)
 {
